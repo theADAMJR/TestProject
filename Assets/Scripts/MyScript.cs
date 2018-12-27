06bab1503b1286fdd59dee16a07f41d750f9d9f5
theADAMJR <adamjf123@gmail.com>
2018-12-27 08:18:40 +0000

    Edit 1
    Test Project Edit 1 - testing CrossPlatformInputManager

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Stating variables
+
+    // Cached component references
+    
+	void Start ()
+    {
+
+	}
+}
